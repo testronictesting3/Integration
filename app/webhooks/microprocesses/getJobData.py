a7612e8f877052d342c2064c800b55bbda26facb
Anthony21 <m.duvenary@gmail.com>
2024-01-18 11:18:13 -0800

    Added the job data module to retreieve job information

--- app/webhooks/microprocesses/getJobData.py
+++ app/webhooks/microprocesses/getJobData.py
@@ -1,2 +1,13 @@
+import sys
+import json
+import requests
+from xytech import Xytech
+
 if __name__=="__main__":
-  print({"This function should get the job data": "Update"})
+    args = sys.argv[1:]
+    job = int(args[0])
+    xy = Xytech()
+    JOB = xy.getJob(job)
+    r = requests.get(url=JOB, auth=(xy.UN, xy.PW))
+    res = r.json()
+    print(json.dumps(res))
