249165fb94b51df54a6e069df3de7372dada63a3
Anthony21 <m.duvenary@gmail.com>
2024-01-29 11:50:23 -0800

    removed blueprint import

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask
 from config import Config
-from app.extensions import database
-#import Blueprints
 from app.main.routes import main as main_bp
 from app.webhooks.routes import webhook as webhook_bp
 from app.boards.routes import board as board_bp
